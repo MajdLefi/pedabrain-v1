d21e37ad72e1e2a6f2d82671cab3247175466b2d
MajdLefi <majd.lefi@gmail.com>
2024-08-27 20:08:30 +0100

    feat(majd): update the api config

--- back-office/src/config/api.js
+++ back-office/src/config/api.js
@@ -1,6 +1,6 @@
 // API URL for general API requests
-const API_URL = 'http://localhost:8000/api/v0'; // Replace this with your actual API URL
-//const API_URL = 'https://pedabrain-v1.onrender.com/api/v0'; // Replace this with your actual API URL
+//const API_URL = 'http://localhost:8000/api/v0'; // Replace this with your actual API URL
+const API_URL = 'https://pedabrain-v1.onrender.com/api/v0'; // Replace this with your actual API URL
 
 // API URL for file uploads
 // const API_URL_UPLOAD = 'http://localhost:8000'; // Replace this with your actual API URL
